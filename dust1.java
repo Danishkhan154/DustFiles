d5b0cdac808304377ab6dc58dc3faa564e5c188f
Danishkhan154 <56692784+Danishkhan154@users.noreply.github.com>
2019-10-17 22:35:41 +0530

    Update dust1.java

--- dust1.java
+++ dust1.java
@@ -1,25 +1,20 @@
 import java.util.*;
 import java.io.*;
 
-class dust1
-{
-    public static void main(String []argh)
-    {
+class dust1{
+    public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         int a=0,b=0,n=0,p=1;
-        for(int i=0;i<t;i++)
-        {
+        for(int i=0;i<t;i++){
              a = in.nextInt();
              b = in.nextInt();
              n = in.nextInt();
         }
-         while(p<=t)
-         {
+         while(p<=t){
            int sum=a; int j=1;
            int l=2*n;
-            while(j<l)
-          {
+            while(j<l){
              sum=sum+(j*b);
              System.out.print(sum+" ");
              j=j*2;
